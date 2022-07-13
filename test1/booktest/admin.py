d8510ce82267f96859f5f5fa6c7a4dea8f2d80c5
aujjh521 <aujjh521@gmail.com>
2022-07-13 22:02:27 +0800

    update from PC

--- test1/booktest/admin.py
+++ test1/booktest/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+#為了讓網頁顯示出models.py裡面有定義的欄位 (attribute)
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'btitle', 'bpub_date'] #id是django會自己產生的
+    list_filter = ['btitle']
+
+admin.site.register(BookInfo, BookInfoAdmin)
+admin.site.register(HeroInfo)
--- test1/booktest/models.py
+++ test1/booktest/models.py
@@ -7,8 +7,16 @@ class BookInfo(models.Model):
     btitle = models.CharField(max_length=20)
     bpub_date = models.DateTimeField()
 
+    #這個是為了讓之後如果透過BookInfo.objects.all()拿到東西會print出內容
+    #不然就只有 [<BookInfo: BookInfo object>]
+    def __str__(self):
+        return self.btitle
+
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=10)
     hgender = models.BooleanField()
     hcontent = models.CharField(max_length=1000)
     hbook = models.ForeignKey(BookInfo) #這邊是希望讓hbook指向一個BookInfo的類別
+
+    def __str__(self):
+        return self.hname
