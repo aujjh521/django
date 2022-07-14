2a4b9a0e7727370d3d524148c6610faa53f11038
aujjh521 <aujjh521@gmail.com>
2022-07-14 21:51:33 +0800

    update from PC

--- test1/booktest/views.py
+++ test1/booktest/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import * # 這是django的 request, response等等package, 以request來講django會幫忙先處理好相關的request資料
 
 from django.template import RequestContext, loader #為了使用template
-from .models import *
+from .models import * #為了從SQL table取資料
 
 # Create your views here.
 def index(request): #意思是server等待外面來的request, 再return response
