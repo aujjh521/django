51e3fb8abb8ff0a5e4da3f6fdd7a6a94a0375c2d
aujjh521 <aujjh521@gmail.com>
2022-07-14 21:40:33 +0800

    update from PC

--- test1/booktest/admin.py
+++ test1/booktest/admin.py
@@ -3,10 +3,26 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import *
+#這個類是為了把HeroInfo鑲嵌到BookInfo裡面去,效果是在admin網頁產生BookInfo的時候可以連帶產生HeroInfo
+class HeroInfoInline(admin.StackedInline):
+    model = HeroInfo
+    extra = 3 #選單裡面起始要出現幾個HeroInfo新增
+
 #為了讓網頁顯示出models.py裡面有定義的欄位 (attribute)
 class BookInfoAdmin(admin.ModelAdmin):
     list_display = ['id', 'btitle', 'bpub_date'] #id是django會自己產生的
-    list_filter = ['btitle']
+    list_filter = ['btitle'] #在admin網頁出現filter要篩選的欄位
+    search_fields = ['btitle'] #在admin網頁出現search要篩選的欄位
+    list_per_page = 5 #每頁出現幾條
+
+    inlines = [HeroInfoInline]
+
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'hname', 'hgender','hcontent','hbook'] #id是django會自己產生的
+    list_filter = ['hname']
+
+
 
 admin.site.register(BookInfo, BookInfoAdmin)
-admin.site.register(HeroInfo)
+admin.site.register(HeroInfo, HeroInfoAdmin)
--- test1/booktest/views.py
+++ test1/booktest/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import * # 這是django的 request, response等等package, 以request來講django會幫忙先處理好相關的request資料
+
+from django.template import RequestContext, loader #為了使用template
+from .models import *
 
 # Create your views here.
+def index(request): #意思是server等待外面來的request, 再return response
+    #temp = loader.get_template('booktest/index.html')
+    #return HttpResponse(temp.render())
+    booklist = BookInfo.objects.all()
+    context = {'booklist': booklist}
+    return render(request, 'booktest/index.html',context) # 透過django本身的render一次執行完讀取template & 渲染並返回http response
