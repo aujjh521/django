360e8f5ddff1b7ba0ba81c1623b7f75c5db5751e
aujjh521 <aujjh521@gmail.com>
2022-07-25 19:43:28 +0800

    update from PC

--- test2/booktest/models.py
+++ test2/booktest/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class BookInfoManager(models.Manager): #繼承models.Manager類別
+    def get_queryset(self): #重寫models.Manager的get_queryset方法 (比原本多出一個.filter(isDelete=False))
+        return super(BookInfoManager, self).get_queryset().filter(isDelete=False)
+
 class BookInfo(models.Model):
     btitle = models.CharField(max_length=20)
     bpub_date = models.DateTimeField(db_column='pub_date') #刻意修改table裡面的欄位名稱(把b拿掉),要注意之後調用還是認attribute name (bpub_date)
@@ -9,6 +14,10 @@ class BookInfo(models.Model):
     isDelete = models.BooleanField(default=False)
     class Meta(): #預設的table name會是<app_name>_<model_name>, 所以應該會是booktest_BookInfo
         db_table = 'bookinfo' #這邊自訂修改表的名子 所以會變成bookinfo
+
+    #使用自訂的manager類別
+    books1 = models.Manager()
+    books2 = BookInfoManager()
 
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=20)
